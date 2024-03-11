8afdca016c1f658d3e86945a0067c68a5787009d
Kevin Ahr <meowmeowahr@gmail.com>
2024-03-10 23:06:43 -0500

    add custom theme option to settings

--- main.py
+++ main.py
@@ -64,6 +64,7 @@ brightness_return_topic: str = mqtt_topics.get("return_brightness_topic",
 
 application_title: str = gui_config.get("title", "NeoPixel Animator")
 app_fullscreen: bool = gui_config.get("fullscreen", False)
+app_custom_theme: bool = gui_config.get("custom_theming", True)
 
 fixed_size_config: dict = gui_config.get("fixed_size", {})
 
@@ -832,11 +833,12 @@ class AnimationWidget(QFrame):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    qta.dark(app)
 
-    with open("style.qss", "r", encoding="utf-8") as qss:
-        app.setStyleSheet(qdarktheme.load_stylesheet() + "\n" + qss.read())
-    QFontDatabase.addApplicationFont("assets/fonts/Cabin/static/Cabin-Regular.ttf")
+    if app_custom_theme:
+        qta.dark(app)
+        with open("style.qss", "r", encoding="utf-8") as qss:
+            app.setStyleSheet(qdarktheme.load_stylesheet() + "\n" + qss.read())
+        QFontDatabase.addApplicationFont("assets/fonts/Cabin/static/Cabin-Regular.ttf")
 
     win = MainWindow()
     sys.exit(app.exec())
